c71f41e324e869b07b593a813fc0363a07d21aab
Peder Antonisen <113884778+ShadySloth@users.noreply.github.com>
2023-03-17 10:38:27 +0100

    login start

--- src/BE/EventCoordinator.java
+++ src/BE/EventCoordinator.java
@@ -1,4 +1,27 @@
 package BE;
 
 public class EventCoordinator {
+    private String Username;
+    private String Password;
+
+    public EventCoordinator(String username, String password) {
+        Username = username;
+        Password = password;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
 }
---
+++ src/GUI/Controller/LoginController.java
@@ -0,0 +1,19 @@
+package GUI.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    private TextField tfUsername;
+    private TextField tfPassword;
+    private Button btnLogin;
+
+    private void login(String user, String password){
+
+    }
+
+    public void handleLogin(ActionEvent actionEvent) {
+
+    }
+}
