1256c639a539be24a3248a55e4cf6041c1088623
monikabrzezinska05 <114471820+monikabrzezinska05@users.noreply.github.com>
2023-03-17 10:49:37 +0100

    CreateEventFXML

--- src/GUI/Controller/CreateEventViewController.java
+++ src/GUI/Controller/CreateEventViewController.java
@@ -1,4 +1,20 @@
 package GUI.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class CreateEventViewController {
+    public Button saveEvent;
+    public Button cancelEvent;
+    public TextField txtNameOfEvent;
+    public TextField txtNumberOfParticipants;
+    public TextField txtLocation;
+    public TextField txtTimeOfEvent;
+
+    public void handleSaveEvent(ActionEvent actionEvent) {
+    }
+
+    public void handleCancelEvent(ActionEvent actionEvent) {
+    }
 }
