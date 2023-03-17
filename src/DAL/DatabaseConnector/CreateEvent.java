35623c5186f97cfa8f54b7a323c935edcc3b40e9
DanKjaer <114471756+DanKjaer@users.noreply.github.com>
2023-03-17 09:42:32 +0100

    createEventDAL

---
+++ src/DAL/DatabaseConnector/CreateEvent.java
@@ -0,0 +1,36 @@
+package DAL.DatabaseConnector;
+
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+
+import java.sql.*;
+
+public class CreateEvent {
+
+    public Event createEvent(String title, Date date, Time time, String location) throws SQLServerException {
+        String sql = "INSERT INTO Event (name, date, time, location)VALUES (?,?,?,?);";
+
+        try (Connection connection = DBConnector.getConnection()){
+            PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            stmt.setString(1, title);
+            stmt.setDate(2, date);
+            stmt.setTime(3, time);
+            stmt.setString(4, location);
+
+            stmt.executeUpdate();
+
+            ResultSet rs = statement.getGeneratedKeys();
+            int id = 0;
+
+            if(rs.next()){
+                id = rs.getInt(1);
+            }
+
+            Event event = new Event(name, date, time, location);
+            return event;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
