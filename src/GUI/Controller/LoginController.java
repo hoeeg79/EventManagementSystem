643a539d9bd2f87d793fdf435aeb9928a0ce1176
Peder Antonisen <113884778+ShadySloth@users.noreply.github.com>
2023-03-17 13:05:14 +0100

    invalid label

--- src/GUI/Controller/LoginController.java
+++ src/GUI/Controller/LoginController.java
@@ -1,5 +1,6 @@
 package GUI.Controller;
 
+import BE.User;
 import GUI.Model.LoginModel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -7,12 +8,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.sql.SQLException;
 
 public class LoginController {
+    @FXML
+    private Label lblWarning;
     @FXML
     private TextField tfUsername;
     @FXML
@@ -22,18 +26,21 @@ public class LoginController {
     private LoginModel loginModel = new LoginModel();
 
     private void login(){
-        String username = tfUsername.getText();
-        String password = tfPassword.getText();
-        Stage primaryStage = (Stage) btnLogin.getScene().getWindow();
-
         try {
-            if (loginModel.login(username, password).isAdmin()){
+            String username = tfUsername.getText();
+            String password = tfPassword.getText();
+            Stage primaryStage = (Stage) btnLogin.getScene().getWindow();
+            User user = loginModel.login(username, password);
+
+            if (user == null){
+                lblWarning.setText("Username or password is invalid.");
+            }else if (user.isAdmin()){
                 Parent root = FXMLLoader.load(getClass().getResource("/GUI/View/MainView.fxml"));
                 Scene scene = new Scene(root);
                 primaryStage.setScene(scene);
                 primaryStage.setTitle("Event System.");
                 primaryStage.show();
-            } else if (!loginModel.login(username, password).isAdmin()) {
+            } else if (!user.isAdmin()) {
                 Parent root = FXMLLoader.load(getClass().getResource("/GUI/View/MainView.fxml"));
                 Scene scene = new Scene(root);
                 primaryStage.setScene(scene);
