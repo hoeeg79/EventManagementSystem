c7257dc0534a7e3579a30eb32e2f3fd606322b31
hoeeg79 <93433669+hoeeg79@users.noreply.github.com>
2023-03-23 10:03:38 +0100

    Events load into table now

--- src/GUI/Model/EventModel.java
+++ src/GUI/Model/EventModel.java
@@ -2,6 +2,8 @@ package GUI.Model;
 
 import BE.Event;
 import BLL.EventManager;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 import java.sql.Date;
 import java.sql.Time;
@@ -9,23 +11,29 @@ import java.sql.Time;
 public class EventModel {
 
     private EventManager eventManager;
+    private ObservableList<Event> eventList;
 
 
-    public EventModel(){
+    public EventModel() throws Exception {
         eventManager = new EventManager();
 
+        eventList = FXCollections.observableArrayList();
+        eventList.addAll(eventManager.getEvents());
     }
 
 
 
     public void createEvent(String name, Date date, Time time, String location) throws Exception{
         Event e = eventManager.createEvent(name, date, time, location);
-
-
+        eventList.add(e);
     }
 
     public void deleteEvent(Event deletedEvent) throws Exception {
         eventManager.deleteEvent(deletedEvent);
+        eventList.remove(deletedEvent);
     }
 
+    public ObservableList<Event> getObservableEvents() {
+        return eventList;
+    }
 }
