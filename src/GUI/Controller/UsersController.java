d209997f7ff3672bd5ae1f0054c9180fd8000dc6
DanKjaer <114471756+DanKjaer@users.noreply.github.com>
2023-03-23 10:01:43 +0100

    User View+Controller

--- src/GUI/Controller/UsersController.java
+++ src/GUI/Controller/UsersController.java
@@ -1,4 +1,15 @@
 package GUI.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
 public class UsersController {
+    public Button btnCreate;
+    public Button btnDelete;
+
+    public void handleCreateCoordinator(ActionEvent actionEvent) {
+    }
+
+    public void handleDeleteCoordinator(ActionEvent actionEvent) {
+    }
 }
