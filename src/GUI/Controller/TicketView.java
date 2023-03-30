d262286bb976d424c01fc867b7442bc238d8d344
DanKjaer <114471756+DanKjaer@users.noreply.github.com>
2023-03-30 09:20:16 +0200

    TicketView

---
+++ src/GUI/Controller/TicketView.java
@@ -0,0 +1,20 @@
+package GUI.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
+public class TicketView {
+    public TableColumn fxEventColumn;
+    public TextField fxName;
+    public TextField fxEmail;
+    public Button btnPrint;
+    public Button btnCancel;
+
+    public void handlePrintTicket(ActionEvent actionEvent) {
+    }
+
+    public void handleCancel(ActionEvent actionEvent) {
+    }
+}
