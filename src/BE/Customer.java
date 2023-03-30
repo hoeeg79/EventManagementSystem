8e3853769f1811fea734ca5f9bdca95087354f78
DanKjaer <114471756+DanKjaer@users.noreply.github.com>
2023-03-30 09:53:57 +0200

    FLERE BUSINESS ENTITIES

---
+++ src/BE/Customer.java
@@ -0,0 +1,24 @@
+package BE;
+
+public class Customer {
+    private String customerName;
+    private String customerEmail;
+
+
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+}
--- src/BE/Tickets.java
+++ src/BE/Tickets.java
@@ -1,4 +1,18 @@
 package BE;
 
-public class Tickets {
+import java.sql.Date;
+import java.sql.Time;
+
+public class Tickets extends Event{
+
+    public Tickets(int id, String name, Date date, Time time, String location) {
+        super(id, name, date, time, location);
+    }
+
+
 }
+
+
+
+// QR code - id of the ticket
+
