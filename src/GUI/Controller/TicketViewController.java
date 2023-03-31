6d242c2a724fcd7b60ec5799015deda2f371cdb8
monikabrzezinska05 <114471820+monikabrzezinska05@users.noreply.github.com>
2023-03-31 08:44:43 +0200

    Made barcode with randomnumbers

--- src/GUI/Controller/TicketViewController.java
+++ src/GUI/Controller/TicketViewController.java
@@ -1,9 +1,7 @@
 package GUI.Controller;
 
 import com.itextpdf.text.*;
-import com.itextpdf.text.pdf.PdfPCell;
-import com.itextpdf.text.pdf.PdfPTable;
-import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.pdf.*;
 import com.itextpdf.text.pdf.draw.LineSeparator;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
@@ -14,8 +12,10 @@ import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Random;
 
 
 public class TicketViewController extends BaseController{
@@ -33,10 +33,14 @@ public class TicketViewController extends BaseController{
 
     }
 
-    public void handlePrintTicket(ActionEvent actionEvent) throws FileNotFoundException, DocumentException {
+    public void handlePrintTicket(ActionEvent actionEvent) throws IOException, DocumentException {
         Document document = new Document(PageSize.A6.rotate());
-        PdfWriter.getInstance(document, new FileOutputStream(fxName.getText() + "_Ticket.pdf"));
+        PdfWriter pdfWriter = PdfWriter.getInstance(document, new FileOutputStream(fxName.getText() + "_Ticket.pdf"));
         document.open();
+
+        /*Image image = Image.getInstance("resources/—Pngtree—3 golden stars_4523648.png");
+        image.scaleAbsolute(PageSize.A6.rotate().getWidth() - image.getScaledWidth() - 10, PageSize.A6.rotate().getHeight() - image.getScaledHeight() - 10);
+        document.add(image);*/
 
         Rectangle background = new Rectangle(0, 0, PageSize.A6.rotate().getWidth(), PageSize.A6.rotate().getHeight());
         background.setBackgroundColor(new com.itextpdf.text.BaseColor(240, 230, 199));
@@ -70,7 +74,28 @@ public class TicketViewController extends BaseController{
         ticketDetails.add(new Paragraph(getModel().getSelectedEvent().getLocation() + " / " + getModel().getSelectedEvent().getTime(), normalFont));
         ticketDetails.add(new Paragraph(String.valueOf(getModel().getSelectedEvent().getId()), normalFont)); //Gonna be a barcode someday
         ticketDetails.add(new Paragraph("Description: This is a test ticket", normalFont));
+        ticketDetails.setSpacingBefore(10);
+        ticketDetails.setAlignment(Element.ALIGN_MIDDLE);
         document.add(ticketDetails);
+
+
+        Random random = new Random();
+        int codeNumber = random.nextInt(1000000);
+        String codeString = Integer.toString(codeNumber);
+
+
+        Barcode128 code128 = new Barcode128();
+        code128.setCode(codeString);
+        code128.setSize(9);
+        code128.setX(2);
+        code128.setN(60);
+        PdfContentByte cb = pdfWriter.getDirectContent();
+        Image barcodeImage = code128.createImageWithBarcode(cb, null, null);
+        float x = PageSize.A6.rotate().getWidth() - barcodeImage.getScaledHeight() - 10;
+        float y = barcodeImage.getScaledHeight() + 10;
+        barcodeImage.setAbsolutePosition(x, y);
+        barcodeImage.setRotationDegrees(90);
+        document.add(barcodeImage);
 
         document.close();
         System.out.println("Ticket generated successfully");
